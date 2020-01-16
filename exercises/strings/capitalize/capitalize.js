caed0cfa1214280f06d312a968f088bd97997473
wismith <56857148+wismith@users.noreply.github.com>
2020-01-15 21:14:16 -0800

    capitalize

--- exercises/strings/capitalize/capitalize.js
+++ exercises/strings/capitalize/capitalize.js
@@ -23,8 +23,8 @@ if (require.main === module) {
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 
-  console.log(capitalize('will'));
-  console.log(capitalize('supercalifragilisticexpialidocious'));
+  console.log(capitalize('will') === 'Will');
+  console.log(capitalize('supercalifragilisticexpialidocious') === 'Supercalifragilisticexpialidocious');
 
 }
 
