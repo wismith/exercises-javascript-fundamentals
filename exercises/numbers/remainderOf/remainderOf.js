4f10b5e54a11d9d6cfb47eb3cf3a32e2bbd56ffb
wismith <56857148+wismith@users.noreply.github.com>
2020-01-16 13:28:20 -0800

    remainder of

--- exercises/numbers/remainderOf/remainderOf.js
+++ exercises/numbers/remainderOf/remainderOf.js
@@ -15,22 +15,19 @@
  * @param {number} num The input number
  * @returns {boolean} True if num is even and false otherwise
  */
-function remainderOf(num1,num2) {
-  /*
-    This is your job. :)
 
+  /*
     If you're not sure, step out of the code and use pen + paper. Start
     with simple examples and pay attention to the process you carry out.
   */
 
-  // First, I have to divide the two numbers
-  let dividend = num1/num2;
-  // Then, I create an if/then based on whether the division is even.
-  if (dividend - Math.floor(dividend) === 0){  // Math.floor() finds the greatest integer of a number
-    return 0;
-  } else {
-    return num1 - num2 * Math.floor(dividend);
+  // I NEED TO WRITE THIS FUNCTION USING ONLY SUBTRACTION AND COMPARISON OPERATORS
+
+function remainderOf(num1, num2) {
+  while (num1 >= 0){
+    num1 -= num2
   }
+  return num1 + num2;
 }
 
 if (require.main === module) {
@@ -39,10 +36,11 @@ if (require.main === module) {
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 
-  console.log(remainderOf(10,2)); // should be 0
-  console.log(remainderOf(10,3)); // should be 1
-  console.log(remainderOf(10,5)); // should be 0
-  console.log(remainderOf(9,2)); // should be 1
+  console.log(remainderOf(10,2) === 0); // should be 0
+  console.log(remainderOf(10,3) === 1); // should be 1
+  console.log(remainderOf(10,5) === 0); // should be 0
+  console.log(remainderOf(9,2) === 1); // should be 1
 }
+
 
 module.exports = remainderOf;
