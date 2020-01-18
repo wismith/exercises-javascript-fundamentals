080d82e0760e42fe2068898a11021d7ccb22665f
wismith <56857148+wismith@users.noreply.github.com>
2020-01-18 15:07:31 -0800

    product of array

--- exercises/arrays/product/product.js
+++ exercises/arrays/product/product.js
@@ -9,7 +9,11 @@
  * @returns {number} The product of the numbers in the array
  */
 function product(array) {
-  // This is your job. :)
+  let runningProduct = array[0];
+  for (let i =1; i < array.length; i++){
+    runningProduct *= array[i];
+  }
+  return runningProduct;
 }
 
 if (require.main === module) {
@@ -17,6 +21,11 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(product([1,2,3,4,5])===120);
+  console.log(product([4,5,6,0])===0);
+  console.log(product([1,1,1,1,1])===1);
+  console.log(product([-2, 3, 4])===-24);
 }
 
 module.exports = product;
