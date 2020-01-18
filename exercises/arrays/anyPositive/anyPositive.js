bda1f39ddcc12a02f3c71648770d99a6e044e936
wismith <56857148+wismith@users.noreply.github.com>
2020-01-18 14:29:10 -0800

    anyPositive arrays

--- exercises/arrays/anyPositive/anyPositive.js
+++ exercises/arrays/anyPositive/anyPositive.js
@@ -14,7 +14,12 @@
  *   and false otherwise.
  */
 function anyPositive(array) {
-  // This is your job. :)
+  for (let num of array){
+    if (num > 0){
+      return true;
+    }
+  }
+  return false;
 }
 
 if (require.main === module) {
@@ -22,6 +27,11 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(anyPositive([1,-2,-3])===true);
+  console.log(anyPositive([0,-1,0])===false);
+  console.log(anyPositive([1,2,3])===true);
+  
 }
 
 module.exports = anyPositive;
