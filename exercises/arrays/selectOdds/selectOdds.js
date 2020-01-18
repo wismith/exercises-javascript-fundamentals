46eabdd96ae85b9bb96403901bdb2fc18b2cb843
wismith <56857148+wismith@users.noreply.github.com>
2020-01-18 15:47:29 -0800

    selectOdds

--- exercises/arrays/selectOdds/selectOdds.js
+++ exercises/arrays/selectOdds/selectOdds.js
@@ -14,7 +14,25 @@
  * @returns {number[]} A new array consisting of all the odd numbers in the input array
  */
 function selectOdds(array) {
-  // This is your job. :)
+  const odds = [];
+  for (let element of array){
+    if (element%2 != 0){
+      odds.push(element);
+    }
+  }
+  return odds;
+}
+
+function outputEqualsExpected(output,expected){
+  if (output.length !== expected.length){
+    return false;
+  }
+  for (let i = 0; i < output.length; i++){
+    if (output[i] !== expected[i]){
+      return false;
+    }
+  }
+  return true;
 }
 
 if (require.main === module) {
@@ -22,6 +40,9 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(outputEqualsExpected(selectOdds([1,2,3,4,5]), [1,3,5]));
+  console.log(outputEqualsExpected(selectOdds([2,4,6,8]),[]));
 }
 
 module.exports = selectOdds;
