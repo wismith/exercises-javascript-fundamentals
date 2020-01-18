0b2d2f9cc1018cdeb252fa24015479f747f8dacc
wismith <56857148+wismith@users.noreply.github.com>
2020-01-18 15:23:50 -0800

    countOdds array

--- exercises/arrays/countOdds/countOdds.js
+++ exercises/arrays/countOdds/countOdds.js
@@ -12,11 +12,22 @@
  * @returns {number} The count of even integers in the array
  */
 function countOdds(array) {
-  // This is your job. :)
+  let oddsCount = 0;
+  for (let element of array){
+    if (element % 2 != 0){
+      oddsCount++;
+    }
+  }
+  return oddsCount;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for countOdds:');
+
+  console.log(countOdds([1,3,5,7,9])===5);
+  console.log(countOdds([1,2,3,4,5])===3);
+  console.log(countOdds([2,4,6,8])===0);
+  
 }
 
 module.exports = countOdds;
