341c5390ce6f93d33bde2e3dddfab174fd473e51
wismith <56857148+wismith@users.noreply.github.com>
2020-01-18 15:21:58 -0800

    countEvens array

--- exercises/arrays/countEvens/countEvens.js
+++ exercises/arrays/countEvens/countEvens.js
@@ -12,7 +12,13 @@
  * @returns {number} The count of even integers in the array
  */
 function countEvens(array) {
-  // This is your job. :)
+  let evensCount = 0;
+  for (let element of array){
+    if (element % 2 === 0){
+      evensCount++;
+    }
+  }
+  return evensCount;
 }
 
 if (require.main === module) {
@@ -20,6 +26,11 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(countEvens([2,3,4,5,6])===3);
+  console.log(countEvens([1,3,5,7,9])===0);
+  console.log(countEvens([4,4,4,4,4])===5);
+
 }
 
 module.exports = countEvens;
