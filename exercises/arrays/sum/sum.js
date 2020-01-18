094955552f8ff826b78b6498b05d5f22f7c664a8
wismith <56857148+wismith@users.noreply.github.com>
2020-01-18 14:44:13 -0800

    sum of array

--- exercises/arrays/sum/sum.js
+++ exercises/arrays/sum/sum.js
@@ -9,7 +9,11 @@
  * @returns {number} The sum of the numbers in the array
  */
 function sum(array) {
-  // This is your job. :)
+  let runningTotal = 0;
+  for (let num of array){
+    runningTotal += num;
+  }
+  return runningTotal;
 }
 
 if (require.main === module) {
@@ -17,6 +21,12 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(sum([1,2,3,4,5])===15);
+  console.log(sum([-1,-2,-3, 1,2,3])===0);
+  console.log(sum([-1,-2,-3])===-6);
+  console.log(sum([0,0,0])===0);
+  
 }
 
 module.exports = sum;
