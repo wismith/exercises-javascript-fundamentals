f94cc1383aaa0b98c5b1d761a3641d9e776d3820
wismith <56857148+wismith@users.noreply.github.com>
2020-01-18 14:25:09 -0800

    allPositive arrays

--- exercises/arrays/allPositive/allPositive.js
+++ exercises/arrays/allPositive/allPositive.js
@@ -12,7 +12,12 @@
  *   and false otherwise.
  */
 function allPositive(array) {
-  // This is your job. :)
+  for (let num of array){
+    if (num <= 0){
+      return false;
+    }
+  }
+  return true;
 }
 
 if (require.main === module) {
@@ -20,6 +25,10 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(allPositive([1,,2,3])===true);
+  console.log(allPositive([-1,3,4])===false);
+  console.log(allPositive([0,1,2])===false);
 }
 
 module.exports = allPositive;
