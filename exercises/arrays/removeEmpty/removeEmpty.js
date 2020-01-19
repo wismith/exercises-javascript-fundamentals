5c55f2d47be998de489bfa1e3baa505ccda22f93
wismith <56857148+wismith@users.noreply.github.com>
2020-01-18 16:23:35 -0800

    removeEmpty

--- exercises/arrays/removeEmpty/removeEmpty.js
+++ exercises/arrays/removeEmpty/removeEmpty.js
@@ -12,14 +12,39 @@
  *   input array.
  */
 function removeEmpty(array) {
-  // This is your job. :)
+  const newArray = [];
+  for (let element of array){
+    if (element){
+      newArray.push(element);
+    }
+    if (element === 0){
+      newArray.push(element);
+    }
+  }
+  return newArray;
 }
 
+function outputEqualsExpected(output,expected){
+  if (output.length !== expected.length){
+    return false;
+  }
+  
+  for (let i = 0; i < output.length; i++){
+    if (output[i] !== expected[i]){
+      return false;
+    }
+  }
+
+  return true;
+}
 if (require.main === module) {
   console.log('Running sanity checks for removeEmpty:');
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(outputEqualsExpected(removeEmpty(['', null, undefined, 'HELLO']), ['HELLO']));
+  console.log(outputEqualsExpected(removeEmpty([0,1,2,3]), [0,1,2,3]));
 }
 
 module.exports = removeEmpty;
