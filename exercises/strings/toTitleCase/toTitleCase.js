b8369c373fdd398d5154a0d84b46354a977e8c37
wismith <56857148+wismith@users.noreply.github.com>
2020-01-18 21:08:55 -0800

    toTitleCase

--- exercises/strings/toTitleCase/toTitleCase.js
+++ exercises/strings/toTitleCase/toTitleCase.js
@@ -17,7 +17,29 @@
  */
 
 function toTitleCase(string) {
-  // This is your job. :)
+  const words = string.split(' ');
+  const wordsNew = [];
+  for (let word of words){
+    let newWord = '';
+    for (let i = 0; i < word.length; i++){
+      let char = word[i];
+      if (i === 0){
+        if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122){
+          newWord += String.fromCharCode(char.charCodeAt() - 32);
+        } else {
+          newWord += char;
+        }
+      } else {
+        if (char.charCodeAt() >= 65 && char.charCodeAt() <= 90){
+          newWord += String.fromCharCode(char.charCodeAt() + 32);
+        } else {
+          newWord += char;
+        }
+      }
+    }
+    wordsNew.push(newWord);
+  }
+  return wordsNew.join(' ');
 }
 
 if (require.main === module) {
@@ -25,6 +47,8 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+  console.log(toTitleCase('a new hope') === 'A New Hope');
+  console.log(toTitleCase('the empire sTRIkes bacK!') === 'The Empire Strikes Back!');
 }
 
 module.exports = toTitleCase;
