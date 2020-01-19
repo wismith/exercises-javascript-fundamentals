1eef8d206157b58e399664eafb1dee8b826ac133
wismith <56857148+wismith@users.noreply.github.com>
2020-01-19 00:22:48 -0800

    printHollowSquare

--- exercises/patterns/printHollowSquare/printHollowSquare.js
+++ exercises/patterns/printHollowSquare/printHollowSquare.js
@@ -28,11 +28,20 @@ let helpers = require('../printHelpers');
  */
 function printHollowSquare(height) {
   for (let i = 0; i < height; i++) {
-    // This is your job. :)
-
+    if (i === 0 || i === height - 1){
+      let numChars = height;
+      helpers.printCountTimes('#', numChars);
+    } else {
+      let numChars = 1;
+      let numSpaces = height - 2;
+      helpers.printCountTimes('#', numChars);
+      helpers.printCountTimes(' ', numSpaces);
+      helpers.printCountTimes('#', numChars);
+    }
     helpers.printNewLine();
-  }
+  }  
 }
+
 /**
  * For testing purposes, prints a diagram of the given height.
  */
